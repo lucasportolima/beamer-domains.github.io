ef4fbae7da856bde4044347f1b921d296adf7e15
Lucas Porto <lucasportolima@live.com>
2022-12-21 13:41:58 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+import 'https://app.getbeamer.com/js/beamer-embed.js'
+
+var beamer_config = {
+            product_id : "gjPUnsMJ51123" //DO NOT CHANGE: This is your product code on Beamer
+        };
